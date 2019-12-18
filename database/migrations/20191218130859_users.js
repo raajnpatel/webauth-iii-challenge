b879d3a0e4f551da3e0d12db712abb1715b3da48
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-18 13:22:16 -0800

    migrations built

--- database/migrations/20191218130859_users.js
+++ database/migrations/20191218130859_users.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+      .createTable('users', user => {
+          user.increments();
+          user.string('username')
+              .unique()
+              .notNullable();
+          user.string('password')
+              .notNullable();
+          user.string('department')
+              .notNullable();
+      })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+      .dropTableIfExists('users');
 };
