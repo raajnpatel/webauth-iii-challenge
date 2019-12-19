46e33694df0c40cc219248a925702e5a217af384
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-19 00:17:17 -0800

    github crashed and caused issues. MVP

--- api/server.js
+++ api/server.js
@@ -2,15 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const userRouter = require('../users/users-router');
-
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
-server.use('/api', userRouter);
 
 
 server.get('/', (req, res) => {
